efef9d6a0145c6db04b45388eb11cfc0dec33dab
mike <michaelwilsonsj@hotmail.com>
2021-01-22 16:41:50 -0500

    setting up bootstrap components

--- src/App.js
+++ src/App.js
@@ -2,18 +2,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <div>
       <header>
         <div className="nav-leader">
+         
           <h3>Mike</h3>
-          <nav className="nav-list">
-            <a className="nav-link active" href="#">Home</a>
-            <a className="nav-link" href="#">Work</a>
-            <a className="nav-link" href="#">Resume</a>
-          </nav>
+            <nav className="nav-list">
+              <a className="nav-link active" href="#">Home</a>
+              <a className="nav-link" href="#">Work</a>
+              <a className="nav-link" href="#">Resume</a>
+            </nav>
         </div>
-
       </header>
+      <div class="card-deck">
+  <div class="card">
+    {/* <img src="..." class="card-img-top" alt="..." /> */}
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    {/* <img src="..." class="card-img-top" alt="..."/> */}
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
     </div>
   );
 }

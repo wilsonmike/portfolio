2e5eb94adf46a48aa5f863137b9b3faf16675643
mike <michaelwilsonsj@hotmail.com>
2021-01-25 17:47:49 -0500

    adding links to social sites

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
-import home from './assets/paper-plane.svg';
-import work from './assets/phone-call.svg';
-import resume from './assets/write.svg'; 
-import contact from './assets/satellite-dish.svg';
+import home from './assets/drag.svg';
+import work from './assets/layers.svg';
+import resume from './assets/paragraph.svg'; 
+import contact from './assets/cloud.svg';
+import logo from './assets/mlogowhite.svg';
+import linkedin from './assets/linkedin.svg';
+import github from './assets/github.svg';
 
 function App() {
   return (
@@ -11,8 +14,9 @@ function App() {
         <section className="glass">
           <div className="dashboard">
             <div className="user">
+              <img className="branding" src={logo} alt="MW Logo"/>
               <h3>Mike Wilson</h3>
-              <p>Dev with design chops</p>
+              <p className="bio">[Dev with design chops]</p>
             </div>
             <div className="links">
             <div className="link">
@@ -32,8 +36,11 @@ function App() {
               <h2>Contact</h2>
             </div>
             </div>
-            <div className="linked">
-              <h2>LinkedIn</h2>
+            <div className="link">
+              <img className="linkedin" src={linkedin} alt="LinkedIn"/>
+              <h3 className="linkin">LinkedIn</h3>
+              <a href="https://github.com/wilsonmike"><img className="linkedin" src={github} alt="GitHub"/></a>
+              <a className="socials" target="_blank" href="https://github.com/wilsonmike"><h3 className="linkin">GitHub</h3></a>
             </div>
           </div>
           <div className="projects">

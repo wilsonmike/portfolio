69b796532de4ad8af62877151cd480a29385fcb8
mike <michaelwilsonsj@hotmail.com>
2021-01-25 18:53:36 -0500

    adding icons for projects

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import contact from './assets/cloud.svg';
 import logo from './assets/mlogowhite.svg';
 import linkedin from './assets/linkedin.svg';
 import github from './assets/github.svg';
+import uncorkd from './assets/cork-icons.svg';
+import study from './assets/open-book.svg';
 
 function App() {
   return (
@@ -39,8 +41,8 @@ function App() {
             <div className="link">
               <img className="linkedin" src={linkedin} alt="LinkedIn"/>
               <h3 className="linkin">LinkedIn</h3>
-              <a href="https://github.com/wilsonmike"><img className="linkedin" src={github} alt="GitHub"/></a>
-              <a className="socials" target="_blank" href="https://github.com/wilsonmike"><h3 className="linkin">GitHub</h3></a>
+              <a rel="noreferrer" href="https://github.com/wilsonmike"><img className="linkedin" src={github} alt="GitHub"/></a>
+              <a rel="noreferrer" className="socials" target="_blank" href="https://github.com/wilsonmike"><h3 className="linkin">GitHub</h3></a>
             </div>
           </div>
           <div className="projects">
@@ -49,6 +51,7 @@ function App() {
             </div>
             <div className="cards">
               <div className="card">
+              <img className="nav-icons" src={uncorkd} alt="UnCorkd"/>
                 <div className="project-info">
                   <h2>UnCorkd</h2>
                   <p>Angular, Firebase, PostgreSQL</p>
@@ -56,16 +59,17 @@ function App() {
                 <h2 className="percentage">Live Project</h2>
               </div>
               <div className="card">
+              <img className="nav-icons" src={study} alt="Study Chat"/>
                 <div className="project-info">
-                  <h2>UnCorkd</h2>
-                  <p>Angular, Firebase, PostgreSQL</p>
+                  <h2>Study Chat</h2>
+                  <p>React, Firebase Realtime Database</p>
                 </div>
                 <h2 className="percentage">Live Project</h2>
               </div>
               <div className="card">
                 <div className="project-info">
-                  <h2>UnCorkd</h2>
-                  <p>Angular, Firebase, PostgreSQL</p>
+                  <h2>Covidly</h2>
+                  <p>React, COVID-19 API, Data Visualization</p>
                 </div>
                 <h2 className="percentage">Live Project</h2>
               </div>

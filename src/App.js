e7631e496cc5e5d9c95947d1b8c975d6f73f419c
mike <michaelwilsonsj@hotmail.com>
2021-01-27 18:13:16 -0500

    added responsive window event

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useEffect, useState } from "react";
 import home from './assets/drag.svg';
 import work from './assets/layers.svg';
 import resume from './assets/paragraph.svg'; 
@@ -12,8 +13,20 @@ import virus from './assets/virus.svg';
 import brunch from './assets/recipe-book.svg';
 
 function App() {
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 1450);
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 1450);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
   return (
     <div>
+    {isDesktop ? (
+      <div>
       <main>
         <section className="glass">
           <div className="dashboard">
@@ -43,7 +56,7 @@ function App() {
             <div className="link">
               <img className="linkedin" src={linkedin} alt="LinkedIn"/>
               <h3 className="linkin">LinkedIn</h3>
-              <a rel="noreferrer" href="https://github.com/wilsonmike"><img className="linkedin" src={github} alt="GitHub"/></a>
+              <a rel="noreferrer" target="_blank" href="https://github.com/wilsonmike"><img className="linkedin" src={github} alt="GitHub"/></a>
               <a rel="noreferrer" className="socials" target="_blank" href="https://github.com/wilsonmike"><h3 className="linkin">GitHub</h3></a>
             </div>
           </div>
@@ -52,6 +65,7 @@ function App() {
               <h1>Completed Projects</h1>
             </div>
             <div className="cards">
+              <a className="project-link" rel="noreferrer" target="_blank" href="https://uncorkd.surge.sh">
               <div className="card">
               <img className="nav-icons" src={uncorkd} alt="UnCorkd"/>
                 <div className="project-info">
@@ -60,6 +74,8 @@ function App() {
                 </div>
                 <h2 className="percentage">Live Project</h2>
               </div>
+              </a>
+              
               <div className="card">
               <img className="nav-icons" src={study} alt="Study Chat"/>
                 <div className="project-info">
@@ -92,10 +108,41 @@ function App() {
       </div>
       <div className="blob2"></div>
     </div>
-      
-    
-  
-  );
+    ) : (
+      <div className="dashboard">
+            <div className="user">
+              <img className="branding" src={logo} alt="MW Logo"/>
+              <h3>Mike Wilson</h3>
+              <p className="bio">[Dev with design chops]</p>
+            </div>
+            <div className="links">
+            <div className="link">
+              <img className="nav-icons" src={home} alt="Home Icon"/>
+              <h2>Home</h2>
+            </div>
+            <div className="link">
+            <img className="nav-icons" src={work} alt="Work Icon"/>
+              <h2>Work</h2>
+            </div>
+            <div className="link">
+            <img className="nav-icons" src={resume} alt="Resume Icon"/>
+              <h2>Resume</h2>
+            </div>
+            <div className="link">
+            <img className="nav-icons" src={contact} alt="Contact Icon"/>
+              <h2>Contact</h2>
+            </div>
+            </div>
+            <div className="link">
+              <img className="linkedin" src={linkedin} alt="LinkedIn"/>
+              <h3 className="linkin">LinkedIn</h3>
+              <a rel="noreferrer" target="_blank" href="https://github.com/wilsonmike"><img className="linkedin" src={github} alt="GitHub"/></a>
+              <a rel="noreferrer" className="socials" target="_blank" href="https://github.com/wilsonmike"><h3 className="linkin">GitHub</h3></a>
+            </div>
+          </div>
+    )}
+  </div>
+);
 }
 
 export default App;

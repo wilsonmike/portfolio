79096233a50daf7b42ffc0652b1db3879a6a9453
mike <michaelwilsonsj@hotmail.com>
2021-01-27 19:03:21 -0500

    added home function to clean up app return and render space

--- src/App.js
+++ src/App.js
@@ -26,88 +26,7 @@ function App() {
   return (
     <div>
     {isDesktop ? (
-      <div>
-      <main>
-        <section className="glass">
-          <div className="dashboard">
-            <div className="user">
-              <img className="branding" src={logo} alt="MW Logo"/>
-              <h3>Mike Wilson</h3>
-              <p className="bio">[Dev with design chops]</p>
-            </div>
-            <div className="links">
-            <div className="link">
-              <img className="nav-icons" src={home} alt="Home Icon"/>
-              <h2>Home</h2>
-            </div>
-            <div className="link">
-            <img className="nav-icons" src={work} alt="Work Icon"/>
-              <h2>Work</h2>
-            </div>
-            <div className="link">
-            <img className="nav-icons" src={resume} alt="Resume Icon"/>
-              <h2>Resume</h2>
-            </div>
-            <div className="link">
-            <img className="nav-icons" src={contact} alt="Contact Icon"/>
-              <h2>Contact</h2>
-            </div>
-            </div>
-            <div className="link">
-              <img className="linkedin" src={linkedin} alt="LinkedIn"/>
-              <h3 className="linkin">LinkedIn</h3>
-              <a rel="noreferrer" target="_blank" href="https://github.com/wilsonmike"><img className="linkedin" src={github} alt="GitHub"/></a>
-              <a rel="noreferrer" className="socials" target="_blank" href="https://github.com/wilsonmike"><h3 className="linkin">GitHub</h3></a>
-            </div>
-          </div>
-          <div className="projects">
-            <div className="status">
-              <h1>Completed Projects</h1>
-            </div>
-            <div className="cards">
-              <a className="project-link" rel="noreferrer" target="_blank" href="https://uncorkd.surge.sh">
-              <div className="card">
-              <img className="nav-icons" src={uncorkd} alt="UnCorkd"/>
-                <div className="project-info">
-                  <h2>UnCorkd</h2>
-                  <p>Angular, Firebase, PostgreSQL Data</p>
-                </div>
-                <h2 className="percentage">Live Project</h2>
-              </div>
-              </a>
-              
-              <div className="card">
-              <img className="nav-icons" src={study} alt="Study Chat"/>
-                <div className="project-info">
-                  <h2>Study Chat</h2>
-                  <p>React, Firebase Realtime Database</p>
-                </div>
-                <h2 className="percentage">Live Project</h2>
-              </div>
-              <div className="card">
-                <img className="nav-icons" src={virus} alt="Virus Icon"/>
-                <div className="project-info">
-                  <h2>Covidly</h2>
-                  <p>React, COVID-19 API, Data Visual</p>
-                </div>
-                <h2 className="percentage">Live Project</h2>
-              </div>
-              <div className="card">
-                <img className="nav-icons" src={brunch} alt="Brunchify logo"/>
-                <div className="project-info">
-                  <h2>Brunchify</h2>
-                  <p>Angular, Edamam API, Recipe App</p>
-                </div>
-                <h2 className="percentage">Live Project</h2>
-              </div>
-            </div>
-          </div>
-        </section>  
-      </main>
-      <div className="blob1">
-      </div>
-      <div className="blob2"></div>
-    </div>
+     <Home />
     ) : (
       <div className="dashboard">
             <div className="user">
@@ -143,6 +62,94 @@ function App() {
     )}
   </div>
 );
+}
+
+
+function Home() {
+  return(
+    <div>
+    <main>
+      <section className="glass">
+        <div className="dashboard">
+          <div className="user">
+            <img className="branding" src={logo} alt="MW Logo"/>
+            <h3>Mike Wilson</h3>
+            <p className="bio">[Dev with design chops]</p>
+          </div>
+          <div className="links">
+          <div className="link">
+            <img className="nav-icons" src={home} alt="Home Icon"/>
+            <h2>Home</h2>
+          </div>
+          <div className="link">
+          <img className="nav-icons" src={work} alt="Work Icon"/>
+            <h2>Work</h2>
+          </div>
+          <div className="link">
+          <img className="nav-icons" src={resume} alt="Resume Icon"/>
+            <h2>Resume</h2>
+          </div>
+          <div className="link">
+          <img className="nav-icons" src={contact} alt="Contact Icon"/>
+            <h2>Contact</h2>
+          </div>
+          </div>
+          <div className="link">
+            <img className="linkedin" src={linkedin} alt="LinkedIn"/>
+            <h3 className="linkin">LinkedIn</h3>
+            <a rel="noreferrer" target="_blank" href="https://github.com/wilsonmike"><img className="linkedin" src={github} alt="GitHub"/></a>
+            <a rel="noreferrer" className="socials" target="_blank" href="https://github.com/wilsonmike"><h3 className="linkin">GitHub</h3></a>
+          </div>
+        </div>
+        <div className="projects">
+          <div className="status">
+            <h1>Completed Projects</h1>
+          </div>
+          <div className="cards">
+            <a className="project-link" rel="noreferrer" target="_blank" href="https://uncorkd.surge.sh">
+            <div className="card">
+            <img className="nav-icons" src={uncorkd} alt="UnCorkd"/>
+              <div className="project-info">
+                <h2>UnCorkd</h2>
+                <p>Angular, Firebase, PostgreSQL Data</p>
+              </div>
+              <h2 className="percentage">Live Project</h2>
+            </div>
+            </a>
+            
+            <div className="card">
+            <img className="nav-icons" src={study} alt="Study Chat"/>
+              <div className="project-info">
+                <h2>Study Chat</h2>
+                <p>React, Firebase Realtime Database</p>
+              </div>
+              <h2 className="percentage">Live Project</h2>
+            </div>
+            <div className="card">
+              <img className="nav-icons" src={virus} alt="Virus Icon"/>
+              <div className="project-info">
+                <h2>Covidly</h2>
+                <p>React, COVID-19 API, Data Visual</p>
+              </div>
+              <h2 className="percentage">Live Project</h2>
+            </div>
+            <div className="card">
+              <img className="nav-icons" src={brunch} alt="Brunchify logo"/>
+              <div className="project-info">
+                <h2>Brunchify</h2>
+                <p>Angular, Edamam API, Recipe App</p>
+              </div>
+              <h2 className="percentage">Live Project</h2>
+            </div>
+          </div>
+        </div>
+      </section>  
+    </main>
+    <div className="blob1">
+    </div>
+    <div className="blob2"></div>
+  </div>
+  )
 }
  
 export default App;
